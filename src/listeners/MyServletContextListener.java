ea022066a51f719e5330459256084805c9c389fc
zemadz <zemadz@gmail.com>
2013-10-14 15:01:25 +0300

    lock

--- src/listeners/MyServletContextListener.java
+++ src/listeners/MyServletContextListener.java
@@ -13,28 +13,31 @@ import dao.SetupDao;
 public class MyServletContextListener implements javax.servlet.ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent arg0) {
-//		SetupDao setup = new SetupDao();
-//		if (!setup.tableExists()) {
-//			setup.createSchema();
-//			setup.insertTestData();
-//		}
-//		File file = new File(System.getProperty("user.home"), "/i377/zemadz/db/db.script");
-//		if(!file.getAbsoluteFile().exists()) {
-//			SetupDao setup = new SetupDao();
-//			setup.createSchema();
-//			setup.insertTestData();
-//		}
+
+		File dbFile = new File(System.getProperty("user.home"), "/i377/zemadz/db/db.script");
+		if (!dbFile.exists()) {
+			SetupDao setup = new SetupDao();
+			setup.createSchema();
+			setup.insertTestData();
+		}
+		// File file = new File(System.getProperty("user.home"),
+		// "/i377/zemadz/db/db.script");
+		// if(!file.getAbsoluteFile().exists()) {
+		// SetupDao setup = new SetupDao();
+		// setup.createSchema();
+		// setup.insertTestData();
+		// }
 	}
 
 	public void contextDestroyed(ServletContextEvent arg0) {
-//		Enumeration<Driver> drivers = DriverManager.getDrivers();
-//		while(drivers.hasMoreElements()) {
-//			try {
-//				DriverManager.deregisterDriver(drivers.nextElement());
-//			} catch (SQLException e) {
-//				e.printStackTrace();
-//			}
-//		}
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			try {
+				DriverManager.deregisterDriver(drivers.nextElement());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
